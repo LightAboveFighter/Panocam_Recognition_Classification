c5ed9585ddddbf7fb58eeb85ac2aa5a101957626
Alexunder <skosarigin@gmail.com>
2025-09-24 17:48:49 +0300

    prepare tracking func for threading

--- source/p1.py
+++ source/p1.py
@@ -1,97 +1,118 @@
 from ultralytics import YOLO
 import cv2 as cv
 from borders import Borders
-import torch
-import yaml
 
 
-def get_tracks(model_name: str, tracker_name: str, video_name: str, save=False) -> list:
+def track_video(
+    model_name: str,
+    tracker_name: str,
+    video: cv.VideoCapture,
+    borders: Borders,
+    new_video: cv.VideoWriter = None,
+    show: bool = False,
+):
+
     model = YOLO(f"materials/trained_models/{model_name}")
-    results = model.track(
-        source=f"materials/in/{video_name}",
-        show=False,
-        persist=True,
-        tracker=tracker_name,
-    )
-    # if save:
-    #     with open(f"materials/out/{video_name}.yaml", "w") as file:
-    #         yaml.dump(results[:100], file)
-    return results
 
+    while video.isOpened():
 
-# def load_tracks(video_name: str) -> list:
-#     with open(f"{video_name}.yaml", "r") as file:
-#         return yaml.load(file)
+        success, frame = video.read()
+        if not success:
+            break
 
+        results = model.track(
+            frame,
+            show=False,
+            persist=True,
+            tracker=tracker_name,
+        )
 
-def save_tracked_video(model_name: str, video_name: str, tracks: list):
-
-    fourcc = cv.VideoWriter_fourcc(*"FMP4")
-    vid_in = cv.VideoCapture(f"materials/in/{video_name}")
-    first_pict = vid_in.read()[1]
-    borders = Borders(
-        "materials/in/borders.yaml", "materials/out/Incident.txt", video_name
-    )
-    # in и out определяются по часовой стрелке от первой точки
-
-    vid = cv.VideoWriter(
-        f"materials/out/{video_name}",
-        fourcc=fourcc,
-        fps=20.0,
-        frameSize=(first_pict.shape[1], first_pict.shape[0]),
-    )
-    vid_in.release()
-
-    model_names = YOLO(f"materials/trained_models/{model_name}").names
-
-    for result in tracks:
-        im = result.orig_img
-
-        people_count = 0
-        if result.boxes is not None and len(result.boxes) > 0:
-            boxes = result.boxes.xyxy.cpu().numpy()
-            ids = result.boxes.id.cpu().numpy() if result.boxes.id is not None else None
-            classes = (
-                result.boxes.cls.cpu().numpy() if result.boxes.id is not None else None
-            )
-
-            point_update_pack = []
-            for i, inf in enumerate(zip(boxes, classes)):
-                box, cls = inf
-
-                if model_names[cls] != "person":
-                    continue
-                people_count += 1
-                x1, y1, x2, y2 = box.astype(int)
-
-                im = cv.rectangle(im, (x1, y1), (x2, y2), (0, 0, 255), 2)
-                center = ((x1 + x2) // 2, (y1 + y2) // 2)
-                im = cv.circle(im, center, 5, (0, 255, 0), -1)
-
-                im = cv.putText(
-                    im,
-                    f"({center[0]}, {center[1]})",
-                    (x2 - 5, y2 - 5),
-                    cv.FONT_HERSHEY_SIMPLEX,
-                    0.5,
-                    (255, 0, 0),
-                    1,
+        for result in results:
+            people_count = 0
+            if result.boxes is not None and len(result.boxes) > 0:
+                boxes = result.boxes.xyxy.cpu().numpy()
+                ids = (
+                    result.boxes.id.cpu().numpy()
+                    if result.boxes.id is not None
+                    else None
+                )
+                classes = (
+                    result.boxes.cls.cpu().numpy()
+                    if result.boxes.id is not None
+                    else None
                 )
 
-                if ids is not None:
-                    point_update_pack.append((int(ids[i]), center))
-            borders.update(point_update_pack)
+                point_update_pack = []
+                for i, inf in enumerate(zip(boxes, classes)):
+                    box, cls = inf
 
-        im = borders.draw(im)
-        im = cv.putText(
-            im,
-            f"{people_count} people",
-            (10, 30),
-            cv.FONT_HERSHEY_SIMPLEX,
-            1,
-            (255, 0, 0),
-            2,
-        )
-        vid.write(im)
+                    if model.names[cls] != "person":
+                        continue
+                    people_count += 1
+                    x1, y1, x2, y2 = box.astype(int)
 
-    vid.release()
+                    frame = cv.rectangle(frame, (x1, y1), (x2, y2), (0, 0, 255), 2)
+                    center = ((x1 + x2) // 2, (y1 + y2) // 2)
+                    frame = cv.circle(frame, center, 5, (0, 255, 0), -1)
+
+                    frame = cv.putText(
+                        frame,
+                        f"({center[0]}, {center[1]})",
+                        (x2 - 5, y2 - 5),
+                        cv.FONT_HERSHEY_SIMPLEX,
+                        0.5,
+                        (255, 0, 0),
+                        1,
+                    )
+
+                    if ids is not None:
+                        point_update_pack.append((int(ids[i]), center))
+                borders.update(point_update_pack)
+
+            frame = borders.draw(frame)
+            frame = cv.putText(
+                frame,
+                f"{people_count} people",
+                (10, 30),
+                cv.FONT_HERSHEY_SIMPLEX,
+                1,
+                (255, 0, 0),
+                2,
+            )
+            if not new_video is None:
+                new_video.write(frame)
+
+        if show:
+            cv.imshow(
+                "YOLO11 Tracking",
+                cv.resize(
+                    frame, (int(frame.shape[1] / 1.2), int(frame.shape[0] / 1.2))
+                ),
+            )
+
+            if cv.waitKey(1) & 0xFF == ord("q"):
+                break
+
+
+model_name = "yolo11n.pt"
+video_name = "Примерочные_2_этаж_двое_в_кабинке_ПЛОХО.avi"
+tracker_name = "bytetrack.yaml"
+
+video_in = cv.VideoCapture(f"materials/in/{video_name}")
+first_pict = video_in.read()[1]
+borders = Borders("materials/in/borders.yaml", "materials/out/Incident.txt", video_name)
+# in и out определяются по часовой стрелке от первой точки
+
+video_out = cv.VideoWriter(
+    f"materials/out/{video_name}",
+    fourcc=cv.VideoWriter_fourcc(*"FMP4"),
+    fps=20.0,
+    frameSize=(first_pict.shape[1], first_pict.shape[0]),
+)
+
+
+track_video(model_name, tracker_name, video_in, borders, video_out, True)
+
+video_out.release()
+video_in.release()
+cv.destroyAllWindows()
